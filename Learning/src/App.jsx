e127c60cb39b8bbd94c6bdd8da57c6fa7c49e983
Dhruv <dhruvsahu7355@gmail.com>
2025-10-30 16:12:34 +0530

    Learning Added

---
+++ Learning/src/App.jsx
@@ -0,0 +1,15 @@
+// function App(){
+//   return "HELLO"
+// }
+// export default App;
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hello from Arrow Function
+    </div>
+  )
+}
+
+export default App
